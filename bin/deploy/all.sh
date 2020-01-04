c856ae090d047a383cf80aecb244e323b4bb1917
Alex Gusev <alex@flancer64.com>
2020-01-04 12:24:48 +0200

    SUPPLZ-420 Create deployment project at github

---
+++ bin/deploy/all.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env/bash
+## =========================================================================
+#   Deploy all components of Supplz VSF
+## =========================================================================
+# shellcheck disable=SC1090 # Can't follow non-constant source.
+# root directory (set before or relative to the current shell script)
+DIR_ROOT=${DIR_ROOT:=$(cd "$(dirname "$0")/../../" && pwd)}
+# load local config and define common functions
+. "${DIR_ROOT}/bin/commons.sh"
+
+
---
+++ cfg.init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# =========================================================================
+#   Deployment configuration template. Copy this file to `cfg.local.sh`
+#   and set actual configuration parameters.
+# =========================================================================
+export CONFIG_IS_LOADED="yes"
+
+export DEBUG_MODE=""       # set "yes" to print commands and their arguments as they are executed
+export FAILSAFE_MODE="yes" # set empty to disable fail safe
