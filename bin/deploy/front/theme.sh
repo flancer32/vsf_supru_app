dfa633ffae97785b454ed022395b115dc6784bfa
Alex Gusev <alex@flancer64.com>
2020-03-26 12:48:29 +0200

    VSFSUPRU-36 Change deployment scheme for theme

--- bin/deploy/front/theme.sh
+++ bin/deploy/front/theme.sh
@@ -19,7 +19,7 @@ DIR_APPS="${DIR_ROOT}/apps"
 DIR_VSF="${DIR_APPS}/vue-storefront"
 DIR_THEME="${DIR_ROOT}/theme"
 DIR_ALL="${DIR_THEME}/all"
-DIR_CODE="${DIR_THEME}/${DIR_THEME_TARGET}"
+DIR_CODE="${DIR_THEME}/${VSF_FRONT_THEME_CODE}"
 DIR_ALL_FILE="${DIR_ALL}/file"
 DIR_ALL_PATCH="${DIR_ALL}/patch"
 DIR_CODE_FILE="${DIR_CODE}/file"
@@ -32,11 +32,6 @@ DIR_THEME_TARGET="${DIR_VSF}/src/themes/supplz"
 info "======================================================================"
 info "Deploy own theme to 'vue-storefront' (code '${VSF_FRONT_THEME_CODE}')."
 info "======================================================================"
-#echo "TEMPORARY CODE TO GET INITITAL THEME:"
-#git clone git@github.com:flancer32/vsf_supru_theme.git "${DIR_THEME_TARGET}"
-#cd "${DIR_THEME_TARGET}" || exit 255
-#git checkout "${VSF_FRONT_THEME_CODE}"
-#echo "END OF TEMPORARY CODE TO GET INITITAL THEME"
 
 info "======================================================================"
 info "Copy own files into 'default' theme."
@@ -50,31 +45,13 @@ info "Apply common patches for 'default' theme."
 info "======================================================================"
 cd "${DIR_ROOT}" || exit 255
 
-##
-# Don't change patches order!!!
-##
-
-info "VSFSUPRU-14: Add separator to product attributes list."
-patch -p0 <"${DIR_ALL_PATCH}/VSFSUPRU-14.patch"
-
-info "VSFSUPRU-16: Missed attributes names in Products comparison."
-patch -p0 <"${DIR_ALL_PATCH}/VSFSUPRU-16.patch"
-
-info "VSFSUPRU-27: Add inc/dec buttons to product details."
-patch -p0 <"${DIR_ALL_PATCH}/VSFSUPRU-27-1.patch"
-patch -p0 <"${DIR_ALL_PATCH}/VSFSUPRU-27-2.patch"
-patch -p0 <"${DIR_ALL_PATCH}/VSFSUPRU-27-3.patch"
-
-info "VSFSUPRU-22: Group prices for customers."
-patch -p0 <"${DIR_ALL_PATCH}/VSFSUPRU-22-1.patch"
-patch -p0 <"${DIR_ALL_PATCH}/VSFSUPRU-22-2.patch"
-
-info "VSFSUPRU-29: Add inc/dec buttons to mini-cart."
-patch -p0 <"${DIR_ALL_PATCH}/VSFSUPRU-29.patch"
-
-info "VSFSUPRU-30: Direct URL for categories."
-patch -p0 <"${DIR_ALL_PATCH}/VSFSUPRU-30-1.patch"
-patch -p0 <"${DIR_ALL_PATCH}/VSFSUPRU-30-2.patch"
+patch -p0 <"${DIR_ALL_PATCH}/components/core/blocks/Form/BaseInputNumber.patch"
+patch -p0 <"${DIR_ALL_PATCH}/components/core/blocks/Microcart/Product.patch"
+patch -p0 <"${DIR_ALL_PATCH}/components/core/ProductAttribute.patch"
+patch -p0 <"${DIR_ALL_PATCH}/components/core/ProductQuantity.patch"
+patch -p0 <"${DIR_ALL_PATCH}/pages/Category.patch"
+patch -p0 <"${DIR_ALL_PATCH}/pages/Compare.patch"
+patch -p0 <"${DIR_ALL_PATCH}/pages/Product.patch"
 
 info "======================================================================"
 info "Apply '${VSF_FRONT_THEME_CODE}' specific patches for 'default' theme."
---
+++ theme/README.md
@@ -0,0 +1,13 @@
+# Theme files
+
+This folder contains stuff for `default` theme modification (see `./bin/deploy/front/theme.sh`).
+
+* `./all/` - common modifications, for both apps;
+** `./file/` - files from these folder overwrite `default` theme files; 
+** `./patch/` - patches for `default` theme files;
+* `./msk/` - modifications for Moscow app;
+** `./file/`
+** `./patch/`
+* `./spb/` - modifications for Saint Petersburg app;
+** `./file/`
+** `./patch/`
